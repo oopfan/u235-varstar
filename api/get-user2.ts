2b1b983a44eb2a19884b6f21735771e1f679187b
oopfan <oopfan@optonline.net>
2020-05-01 10:26:33 -0400

    Access GitHub Pages docs from Serverless Function

---
+++ api/get-user2.ts
@@ -0,0 +1,10 @@
+import { NowRequest, NowResponse } from '@now/node'
+const got = require('got');
+
+// I need error handling!
+export default (req: NowRequest, res: NowResponse) => {
+  (async () => {
+    const body = await got('https://oopfan.github.io/u235-vstar/testing.json').json();
+    res.json(body);
+  })();
+}
