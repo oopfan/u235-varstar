050b67751d466b334d19640a2a8debe16194b0ec
oopfan <oopfan@optonline.net>
2020-05-01 21:19:16 -0400

    Add observations for YZ Boo and GK Boo

--- api/get-user2.ts
+++ api/get-user2.ts
@@ -4,7 +4,7 @@ const got = require('got');
 // I need error handling!
 export default (req: NowRequest, res: NowResponse) => {
   (async () => {
-    const body = await got('https://oopfan.github.io/u235-vstar/testing.json').json();
+    const body = await got('https://oopfan.github.io/u235-vstar/gk-boo.json').json();
     res.json(body);
   })();
 }
