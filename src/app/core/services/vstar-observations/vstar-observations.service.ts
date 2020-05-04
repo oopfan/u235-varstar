235511c3dec356b98bd7ef3d26460ec93c4b5cc1
oopfan <oopfan@optonline.net>
2020-05-04 18:38:43 -0400

    Add cache to services

--- src/app/core/services/vstar-observations/vstar-observations.service.ts
+++ src/app/core/services/vstar-observations/vstar-observations.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VstarObservationsService {
+  cache = {};
 
   constructor(private http: HttpClient) { }
 
   get(id: string) {
-    return this.http.get('https://oopfan.github.io/u235-vstar/' + id + '.json');
+    if (this.cache[id] !== undefined) {
+      return new Observable(subscriber => {
+        subscriber.next(this.cache[id]);
+      });
+    }
+    return this.http.get('https://oopfan.github.io/u235-vstar/' + id + '.json').pipe(map(value => {
+      this.cache[id] = value;
+      return value;
+    }));
   }
 }
--- src/app/core/services/vstar-overview/vstar-overview.service.ts
+++ src/app/core/services/vstar-overview/vstar-overview.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VstarOverviewService {
+  cache = null;
 
   constructor(private http: HttpClient) { }
 
   get(id: string) {
-    return this.http.get('https://oopfan.github.io/u235-vstar/dir.json').pipe(map(value => value[id]));
+    if (this.cache) {
+      return new Observable(subscriber => {
+        subscriber.next(this.cache[id]);
+      });
+    }
+    return this.http.get('https://oopfan.github.io/u235-vstar/dir.json').pipe(map(value => {
+      this.cache = value;
+      return value[id];
+    }));
   }
 }
