f6802f63702e3caca809387b8124c7cdd983ae88
oopfan <oopfan@optonline.net>
2020-05-07 17:59:23 -0400

    Choose to use shorter route names

--- src/app/varstars/varstars-routing.module.ts
+++ src/app/varstars/varstars-routing.module.ts
@@ -4,10 +4,10 @@ import * as fromComponents from './components';
 
 export const routes: Routes = [
   { path: 'varstars', component: fromComponents.VarStarsHomeComponent },
-  { path: 'varstar-overview/:id', component: fromComponents.VarStarOverviewComponent },
-  { path: 'varstar-observations/:id', component: fromComponents.VarStarObservationsComponent },
-  { path: 'varstar-light-curve/:id', component: fromComponents.VarStarLightCurveComponent },
-  { path: 'varstar-phase-plot/:id', component: fromComponents.VarStarPhasePlotComponent },
+  { path: 'overview/:id', component: fromComponents.VarStarOverviewComponent },
+  { path: 'observations/:id', component: fromComponents.VarStarObservationsComponent },
+  { path: 'light-curve/:id', component: fromComponents.VarStarLightCurveComponent },
+  { path: 'phase-plot/:id', component: fromComponents.VarStarPhasePlotComponent },
 ];
 
 @NgModule({
